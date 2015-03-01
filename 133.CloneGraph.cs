2ac79e3744a2ca2400915929812cbbd734e2fad8
xieqilu <xieqilu1989@gmail.com>
2015-02-28 18:14:41 -0800

    Update 133.CloneGraph.cs

--- 133.CloneGraph.cs
+++ 133.CloneGraph.cs
@@ -14,3 +14,63 @@ public class Solution {
 }
 
 Driver Code:
+
+class __Driver__ {
+    private static UndirectedGraphNode IsCopied(UndirectedGraphNode A, UndirectedGraphNode B) {
+        if (A == null || B == null) return null;
+        
+        HashSet<UndirectedGraphNode> hash = new HashSet<UndirectedGraphNode>();
+        List<UndirectedGraphNode> que = new List<UndirectedGraphNode>();
+        int head = 0;
+        hash.Add(A);
+        que.Add(A);
+        while (head < que.Count) {
+            UndirectedGraphNode now = que[head];
+            foreach (UndirectedGraphNode neighbor in now.neighbors)
+            {
+                if (hash.Add(neighbor)) {
+                    que.Add(neighbor);
+                }
+            }
+            head++;
+        }
+        
+        HashSet<UndirectedGraphNode> hashB = new HashSet<UndirectedGraphNode>();
+        que.Clear();
+        head = 0;
+        hashB.Add(B);
+        que.Add(B);
+        while (head < que.Count) {
+            UndirectedGraphNode now = que[head];
+            if (hash.Contains(now)) return now;
+            for (UndirectedGraphNode neighbor : now.neighbors)
+            {
+                if (hashB.Add(neighbor)) {
+                    que.Add(neighbor);
+                }
+            }
+            head++
+        }
+        return null;
+    }
+
+  static void Main(string[] args) {
+  using (StreamWriter file = new StreamWriter("user.out")) {
+    file.AutoFlush = true;
+    string line;
+    while ((line = Console.ReadLine()) != null) {
+        UndirectedGraphNode node = UndirectedGraphNode.deserialize(s);
+        UndirectedGraphNode copied = new Solution().CloneGraph(node);
+        UndirectedGraphNode same = IsCopied(node, copied);
+            if (same == null) {
+                file.WriteLine(__Serializer__.serialize(copied));
+            } else {
+                file.WriteLine("Node with label " + same.label + " was not copied but a reference to the original one.");
+            }
+    }
+  }
+  }
+}
+
+
+ac Solution Code:
